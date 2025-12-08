677944cf7e0a1d243da1376503a4bbee8face2e5
SaurabhShisode <119099005+SaurabhShisode@users.noreply.github.com>
2025-12-08 18:54:10 +0530

    added env

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,14 +1,9 @@
 const admin = require('firebase-admin');
 
-let app;
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-  app = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
-} else {
-  app = admin.app();
-}
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 module.exports = admin;
